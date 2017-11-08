3dee5c02ec416ecfbbcedb5a64f8c30de3db14b5
jerichen <jeri.chen0805@yahoo.com.tw>
2017-11-08 12:03:32 +0800

    change jerichen images

--- nginx/scripts/make_ssl.sh
+++ nginx/scripts/make_ssl.sh
@@ -9,4 +9,3 @@ mkdir -p "$SSL_DIR"
 # does not exist 80.443 port config
 echo "--- Copying $file SSL crt and key ---"
 openssl req -nodes -new -x509 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=US/ST=NY/L=NYC/O=Dis/CN=NGINX"
-
